7e0d509e091c69fb7bd96a6cab004a3bf442e2bf
Tarciso Loiola <tarcisoloiola@gmail.com>
2018-11-30 05:06:36 -0500

    Adding new Components to frontend designer

--- client/src/components/Pages/Home/Home.js
+++ client/src/components/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Home extends Component{
 
@@ -10,6 +11,9 @@ class Home extends Component{
                 <div className='ipp-home-helloworld'>
                     Hello World Home!
                 </div>
+
+                <Link to='Admin'>Admin</Link>
+                
 
             </div>
         )
