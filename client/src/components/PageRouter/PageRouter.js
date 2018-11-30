5df60fdb31c78d35c7d69df9ff95f9fe80b4125f
Tarciso Loiola <tarcisoloiola@gmail.com>
2018-11-30 04:31:09 -0500

    Subir_Templates #1 /  npm install ou yarn to install dependencies

---
+++ client/src/components/PageRouter/PageRouter.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import NavigationBar from '../NavigationBar/NavigationBar';
+import ContactUs from '../Pages/ContactUs/ContactUs';
+import Home from '../Pages/Home/Home';
+import Admin from '../Pages/Admin/Admin';
+
+class PageRouter extends Component{
+    render(){
+        return(
+            <div>
+
+                <Route exact path = '/' component = { Home } />
+                <Route path = '/admin' component = { Admin } />
+                
+            </div>
+            )
+    }
+}
+
+export default PageRouter;
+
+
+// <aside className = 'lf-page-router-aside-container'>
+// </aside>
